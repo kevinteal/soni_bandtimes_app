2ed004f5488b2539211283f5a8ada4f653979a2d
kevinteal <kevin.teal1@gmail.com>
2014-06-28 18:32:34 +0100

    continue development
    more dynamic additions mainly need database now to continue further

--- soni_script.js
+++ soni_script.js
@@ -12,26 +12,61 @@ var d=	$(e).val();
 //
 
 function change_stage(stage){
-	
-	switch(stage){
-		case 1:
-		alert("main");
 		//change stage heading at top of page
 		//clear content in all tabs
 		//add new content to all tabs
+	var stage_name;
+	
+	switch(stage){
+		case 1:
+		//alert("apollo");
+		stage_name= "Apollo";
 		break;
 		case 2:
-		alert("second");
+		//alert("saturn");
+		stage_name= "Saturn";
 		break;
 		case 3:
-		alert("bohemia");
+		//alert("bohemia");
+		stage_name = "Bohemia";
 		break;
 		case 4:
-		alert("red bull");
-		break;
-		case 5:
-		alert("jager");
+		//alert("jager");
+		stage_name = "Jagermeister";
 		break;
 	}
+	//change stage header
+	$("#stage_name_heading").text(stage_name);
+	//remove content in tabs
+	$("#tab_one,#tab_two,#tab_three").empty();
+	//add new content to all tabs
+	//execute query for stage, order bands into day and desc time
+var name ="notsaturn";
+	if(stage_name=="Saturn"){
+		name="kevin";
+	}
+	
+	//friday
+	for(var i=0;i<10;i++){
+	$("#tab_one").append("<div class='lineup_band' ><strong>BAND NAME"+i+name+"</strong>"+
+	"<br/><span class='darker_text'>START TIME - END TIME</span><br/>"+
+	"<form><select name='flip"+i+"' id='flip"+i+"' data-role='flipswitch' data-mini='true' data-theme='c' onChange='add_to_plan(this,'bandid"+i+"')'><option value='off'>Off</option> <option value='on'>On</option></select></form> </div>");
+	}
+	//saturday
+	for(var ii=0;ii<10;ii++){
+	$("#tab_two").append("<div class='lineup_band' ><strong>BAND NAME"+ii+name+"</strong>"+
+	"<br/><span class='darker_text'>START TIME - END TIME</span><br/>"+
+	"<form><select name='flip"+ii+"' id='flip"+ii+"' data-role='flipswitch' data-mini='true' data-theme='c' onChange='add_to_plan(this,'bandid"+ii+"')'><option value='off'>Off</option> <option value='on'>On</option></select></form> </div>");
+	}
+	//sunday
+	for(var iii=0;iii<10;iii++){
+	$("#tab_three").append("<div class='lineup_band' ><strong>BAND NAME"+iii+name+"</strong>"+
+	"<br/><span class='darker_text'>START TIME - END TIME</span><br/>"+
+	"<form><select name='flip"+iii+"' id='flip"+iii+"' data-role='flipswitch' data-mini='true' data-theme='c' onChange='add_to_plan(this,'bandid"+iii+"')'><option value='off'>Off</option> <option value='on'>On</option></select></form> </div>");
+	}
+	
+	
+	$(".lineup_band").trigger('create');
 	$("#stage_panel").panel("close");
+	$("#tabs").trigger('updatelayout');
 }
